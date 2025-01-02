0fb17111229693ec450554e974393ef500ae32f4
sara <saraamirabadifarahani82@gmail.com>
2025-01-02 16:39:46 +0330

    profile

---
+++ js/change.js
@@ -0,0 +1,32 @@
+
+function closeWelcomeBox() {
+    document.getElementById('welcome-box').style.display = 'none';
+}
+
+function showTooltip(event) {
+    var tooltip = document.getElementById('tooltip');
+    tooltip.style.display = 'block'; 
+    tooltip.style.left = event.pageX + 'px';
+    tooltip.style.top = (event.pageY + 20) + 'px'; 
+}
+
+function hideTooltip() {
+    var tooltip = document.getElementById('tooltip');
+    tooltip.style.display = 'none'; 
+}
+
+
+    document.querySelector('.btn button').addEventListener('click', function(event) {
+        event.preventDefault(); 
+
+        const password = document.getElementById('password').value;
+        const confirmPassword = document.getElementById('confirm-password').value;
+
+        if (password !== confirmPassword) {
+            alert('رمز عبور و تکرار آن مطابقت ندارد.');
+            return false; 
+        }
+        else {
+            window.location.href = '../html/change.html'; 
+        }
+    });
